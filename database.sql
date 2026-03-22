8b8d691561aa8a56bb4f98e9fc3858955c38ec23
QistinaRoslanQR <tinaroslan@gmail.com>
2026-03-22 16:40:32 +0800

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE IF NOT EXISTS morphdigi;
 USE morphdigi;
 
+
 CREATE TABLE IF NOT EXISTS transactions (
     id INT AUTO_INCREMENT PRIMARY KEY,
     currency_code VARCHAR(10) NOT NULL,
